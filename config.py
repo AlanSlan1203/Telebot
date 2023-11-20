3ddeb124f4443fe64c32e9046f2860288be9cc4f
AlanSlan1203 <slanalan@mail.ru>
2023-11-20 21:08:36 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = '6204647671:AAExawcnbtpPa7tCwkWJ2vqVVmM8s9yDjjY'
+
+SQLALCHEMY_URL = "sqlite+aiosqlite:///dp.sqlite3"
---
+++ main.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+import sys
+import logging
+
+
+from config import TOKEN
+from app.handlers import router
+from app.database.models import async_main
+
+
+async def main():
+    await async_main()
+
+    bot = Bot(token=TOKEN, parse_mode='HTML')
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
